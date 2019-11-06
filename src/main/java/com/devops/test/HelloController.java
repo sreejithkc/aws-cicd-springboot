137332f9667c68518c0c776c7ec596b7fd8bb2d7
sreejithkc <sreejithk11@gmail.com>
2019-11-07 00:30:31 +0530

    build file corrected 4

--- src/main/java/com/devops/test/HelloController.java
+++ src/main/java/com/devops/test/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
-
+@GetMapping
     public String helloWorld(){
         return "hello aws";
     }
